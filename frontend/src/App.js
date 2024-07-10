974f3059bea674238a18378013f4251888c4ea6b
AryamannMishraFR <fr.aryamann.mishra@prolim.in>
2024-07-10 16:46:32 +0530

    tracker page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import PersonalEvaluationForm from './components/PersonalEvaluationForm';
 import HealthMetricsForm from './components/HealthMetricsForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
+import Tracker from './components/Tracker';
 import './App.css'
 import WellnessData from './components/WellnessData';
 import Tracker from './components/Tracker';
